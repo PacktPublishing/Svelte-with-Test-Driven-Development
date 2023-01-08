52a2960bfcad944b944fd73c6ca4b77be568150a
Daniel Irvine <daniel@danielirvine.com>
2023-01-08 21:23:28 +0000

    Add a test for the DOM

--- src/index.test.js
+++ src/index.test.js
@@ -4,4 +4,12 @@ describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
 	});
+
+	it('renders hello into the document', () => {
+		document.body.innerHTML =
+			'<h1>Hello, world!</h1>';
+		expect(document.body).toHaveTextContent(
+			'Hello, world!'
+		);
+	});
 });
