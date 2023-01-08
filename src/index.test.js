146f14ae52c9e571ae5196892029dd4389041ad4
Daniel Irvine <daniel@danielirvine.com>
2023-01-08 21:35:28 +0000

    Add a test for a basic Svelte component

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,6 @@
 import { describe, it, expect } from 'vitest';
+import { render } from '@testing-library/svelte';
+import Hello from './Hello.svelte';
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
@@ -6,10 +8,12 @@ describe('sum test', () => {
 	});
 
 	it('renders hello into the document', () => {
-		document.body.innerHTML =
-			'<h1>Hello, world!</h1>';
-		expect(document.body).toHaveTextContent(
-			'Hello, world!'
-		);
+		render(Hello, { name: 'world' });
+		expect(document.body).toHaveTextContent('Hello, world!');
+	});
+
+	it('renders hello, svelte', () => {
+		render(Hello, { name: 'Svelte' });
+		expect(document.body).toHaveTextContent('Hello, Svelte!');
 	});
 });
