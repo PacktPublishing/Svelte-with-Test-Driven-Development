0bec222937fa98ee62b42f68c56e16ba29d9bb52
Daniel Irvine <daniel@danielirvine.com>
2023-01-15 16:50:48 +0000

    Add a Playwright test for saving a new birthday

--- tests/birthdays.test.js
+++ tests/birthdays.test.js
@@ -9,3 +9,15 @@ test('lists all birthday', async ({ page }) => {
 		page.getByText('Athena')
 	).toBeVisible();
 });
+
+test('saves a new birthday', async ({ page }) => {
+	await page.goto('/birthdays');
+	await page.getByLabel('Name').fill('Persephone');
+	await page
+		.getByLabel('Date of birth')
+		.fill('1985-01-01');
+	await page.getByRole('button').click();
+	await expect(
+		page.getByText('Persephone')
+	).toBeVisible();
+});
