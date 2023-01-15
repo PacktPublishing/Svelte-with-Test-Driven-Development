e5910ab208fd3787f2c6e733ec2cff4d4074807b
Daniel Irvine <daniel@danielirvine.com>
2023-01-15 21:27:06 +0000

    Add Playwright test for editing a birthday

--- tests/birthdays.test.js
+++ tests/birthdays.test.js
@@ -16,7 +16,9 @@ test('saves a new birthday', async ({ page }) => {
 	await page
 		.getByLabel('Date of birth')
 		.fill('1985-01-01');
-	await page.getByRole('button').click();
+	await page
+		.getByRole('button', { name: 'Save' })
+		.click();
 	await expect(
 		page.getByText('Persephone')
 	).toBeVisible();
@@ -30,7 +32,9 @@ test('does not save a birthday if there are validation errors', async ({
 	await page
 		.getByLabel('Date of birth')
 		.fill('invalid');
-	await page.getByRole('button').click();
+	await page
+		.getByRole('button', { name: 'Save' })
+		.click();
 	await expect(
 		page.getByText('Demeter')
 	).not.toBeVisible();
@@ -39,4 +43,48 @@ test('does not save a birthday if there are validation errors', async ({
 			'Please provide a date of birth in the YYYY-MM-DD format.'
 		)
 	).toBeVisible();
+});
+
+test('edits a birthday', async ({ page }) => {
+	await page.goto('/birthdays');
+	// add a birthday using the form
+	await page.getByLabel('Name').fill('Ares');
+	await page
+		.getByLabel('Date of birth')
+		.fill('1985-01-01');
+	await page
+		.getByRole('button', { name: 'Save' })
+		.click();
+
+	// find the Edit button for that person
+	await page
+		.getByRole('listitem')
+		.filter({ hasText: 'Ares' })
+		.getByRole('button', { name: 'Edit' })
+		.click();
+
+	// find the text box with label Date of Birthday
+	// change the date of birth
+	await page
+		.getByLabel('Date of birth')
+		.fill('1995-01-01');
+
+	// click save
+	await page
+		.getByRole('button', { name: 'Save' })
+		.click();
+
+	// check that the original text doesn't appear
+	await expect(
+		page
+			.getByRole('listitem')
+			.filter({ hasText: 'Ares' })
+	).not.toContainText('1985-01-01');
+
+	// check that the new text does appear
+	await expect(
+		page
+			.getByRole('listitem')
+			.filter({ hasText: 'Ares' })
+	).toContainText('1995-01-01');
 });
