dffb7efeedc749ce93904587280ecb407020aa3a
Daniel Irvine <daniel@danielirvine.com>
2023-01-15 15:17:04 +0000

    Add a BirthdayForm component

---
+++ src/routes/birthdays/BirthdayForm.svelte
@@ -0,0 +1,13 @@
+<form method="post" name="birthday">
+	<label>
+		Name
+		<input type="text" name="name" />
+	</label>
+
+	<label>
+		Date of birth
+		<input type="text" name="dob" />
+	</label>
+
+	<input type="submit" value="Save" />
+</form>
