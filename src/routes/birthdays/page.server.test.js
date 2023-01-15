63f11d275950f87564f5d26e9e89bf97dfc58485
Daniel Irvine <daniel@danielirvine.com>
2023-01-15 15:49:11 +0000

    Add a form action request factory, and a skipped test

--- src/routes/birthdays/page.server.test.js
+++ src/routes/birthdays/page.server.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest';
-import { load } from './+page.server.js';
+import { createFormDataRequest } from 'src/factories/formDataRequest.js';
+import { load, actions } from './+page.server.js';
 
 describe('/birthdays - load', () => {
 	it('returns a fixture of two items', () => {
@@ -8,5 +9,23 @@ describe('/birthdays - load', () => {
 			{ name: 'Hercules', dob: '1994-02-02' },
 			{ name: 'Athena', dob: '1989-01-01' }
 		]);
+	});
+});
+
+describe('/birthdays - default action', () => {
+	it.skip('adds a new birthday into the list', async () => {
+		const request = createFormDataRequest({
+			name: 'Zeus',
+			dob: '2009-02-02'
+		});
+
+		await actions.default({ request });
+
+		expect(load().birthdays).toContainEqual(
+			expect.objectContaining({
+				name: 'Zeus',
+				dob: '2009-02-02'
+			})
+		);
 	});
 });
