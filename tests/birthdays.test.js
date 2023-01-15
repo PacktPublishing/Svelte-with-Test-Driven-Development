999dbdec514405c3955e6ef6ad41fa8addfcae7c
Daniel Irvine <daniel@danielirvine.com>
2023-01-15 16:56:13 +0000

    Add a Playwright test for a validation error

--- tests/birthdays.test.js
+++ tests/birthdays.test.js
@@ -21,3 +21,22 @@ test('saves a new birthday', async ({ page }) => {
 		page.getByText('Persephone')
 	).toBeVisible();
 });
+
+test('does not save a birthday if there are validation errors', async ({
+	page
+}) => {
+	await page.goto('/birthdays');
+	await page.getByLabel('Name').fill('Demeter');
+	await page
+		.getByLabel('Date of birth')
+		.fill('invalid');
+	await page.getByRole('button').click();
+	await expect(
+		page.getByText('Demeter')
+	).not.toBeVisible();
+	await expect(
+		page.getByText(
+			'Please provide a date of birth in the YYYY-MM-DD format.'
+		)
+	).toBeVisible();
+});
