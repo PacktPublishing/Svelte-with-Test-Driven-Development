c3b9cb3debed23e18d6e71078c248bca453ebc23
Daniel Irvine <daniel@danielirvine.com>
2023-01-18 11:06:31 +0000

    Set the Vite reporter to verbose

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		reporter: 'verbose'
 	}
 };
 
