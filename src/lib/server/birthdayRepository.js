7cf00ab74188aa38267dcd4c60146c6010322d8c
Daniel Irvine <daniel@danielirvine.com>
2023-01-21 12:49:03 +0000

    Reimplement birthday store as a Map

--- src/lib/server/birthdayRepository.js
+++ src/lib/server/birthdayRepository.js
@@ -1,10 +1,12 @@
 import { randomUUID } from 'crypto';
 
-let db = [];
+const db = new Map();
 
-export const addNew = (item) =>
-	db.push({ ...item, id: randomUUID() });
+export const addNew = (item) => {
+	const id = randomUUID();
+	db.set(id, { ...item, id });
+};
 
-export const getAll = () => Array.from(db);
+export const getAll = () => Array.from(db.values());
 
-export const clear = () => (db = []);
+export const clear = () => db.clear();
