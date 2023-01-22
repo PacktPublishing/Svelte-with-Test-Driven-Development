6036cc2ac96c63e0d887d514769d0c1d221b3d7b
Daniel Irvine <daniel@danielirvine.com>
2023-01-22 16:17:36 +0000

    Re-implement load using a mutable array

--- src/routes/birthdays/+page.server.js
+++ src/routes/birthdays/+page.server.js
@@ -1,6 +1,10 @@
+const db = [];
+
+const addNew = (item) => db.push(item);
+
+addNew({ name: 'Hercules', dob: '1994-02-02' });
+addNew({ name: 'Athena', dob: '1989-01-01' });
+
 export const load = () => ({
-	birthdays: [
-		{ name: 'Hercules', dob: '1994-02-02' },
-		{ name: 'Athena', dob: '1989-01-01' }
-	]
+	birthdays: Array.from(db)
 });
