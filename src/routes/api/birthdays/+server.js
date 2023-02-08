46c6d8f3765cb055b7628c48630d60642e60b72f
Daniel Irvine <daniel@danielirvine.com>
2023-02-08 16:32:44 +0000

    Add API endpoints for adding and replacing birthdays

--- src/routes/api/birthdays/+server.js
+++ src/routes/api/birthdays/+server.js
@@ -4,5 +4,13 @@ import {
 } from '$lib/server/birthdayRepository.js';
 import { json, error } from '@sveltejs/kit';
 
+export const POST = async ({ request }) => {
+	const { name, dob } = await request.json();
+	const result = addNew({ name, dob });
+	if (result.error) throw error(422, result.error);
+
+	return json(result);
+};
+
 export const GET = () =>
 	json({ birthdays: getAll() });
--- src/routes/api/birthdays/server.test.js
+++ src/routes/api/birthdays/server.test.js
@@ -1,14 +1,24 @@
-import { describe, it, expect } from 'vitest';
-import { GET } from './+server.js';
-import { addNew } from '$lib/server/birthdayRepository.js';
+import {
+	describe,
+	it,
+	expect,
+	beforeEach
+} from 'vitest';
+import * as birthdayRepository from '$lib/server/birthdayRepository.js';
 import { createBirthday } from 'src/factories/birthday.js';
+import { createRequest } from 'src/factories/request.js';
+import { GET, POST } from './+server.js';
 
 const bodyOfResponse = (response) => response.json();
 
 describe('GET', () => {
 	it('returns all the birthdays from the store', async () => {
-		addNew(createBirthday('Hercules', '2010-04-05'));
-		addNew(createBirthday('Ares', '2008-03-02'));
+		birthdayRepository.addNew(
+			createBirthday('Hercules', '2010-04-05')
+		);
+		birthdayRepository.addNew(
+			createBirthday('Ares', '2008-03-02')
+		);
 		const { birthdays } = await bodyOfResponse(GET());
 		expect(birthdays).toEqual([
 			expect.objectContaining(
@@ -18,5 +28,51 @@ describe('GET', () => {
 				createBirthday('Ares', '2008-03-02')
 			)
 		]);
+	});
+});
+
+describe('POST', () => {
+	beforeEach(birthdayRepository.clear);
+
+	it('saves the birthday in the store', async () => {
+		await POST({
+			request: createRequest(
+				createBirthday('Hercules', '2009-03-01')
+			)
+		});
+		expect(birthdayRepository.getAll()).toHaveLength(
+			1
+		);
+		expect(birthdayRepository.getAll()[0]).toContain(
+			createBirthday('Hercules', '2009-03-01')
+		);
+	});
+
+	it('returns a json response with the data', async () => {
+		const response = await POST({
+			request: createRequest(
+				createBirthday('Hercules', '2009-03-01')
+			)
+		});
+		expect(await bodyOfResponse(response)).toContain(
+			createBirthday('Hercules', '2009-03-01')
+		);
+	});
+
+	it('throws an error if the data is invalid', async () => {
+		expect.hasAssertions();
+		try {
+			await POST({
+				request: createRequest(
+					createBirthday('Ares', '')
+				)
+			});
+		} catch (error) {
+			expect(error.status).toEqual(422);
+			expect(error.body).toEqual({
+				message:
+					'Please provide a date of birth in the YYYY-MM-DD format.'
+			});
+		}
 	});
 });
